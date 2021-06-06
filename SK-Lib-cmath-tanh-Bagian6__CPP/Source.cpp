f7807aa50467e51a0b2d5f1cfe5d6a861da90632
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-06 12:59:03 +0700

    Add project files.

---
+++ SK-Lib-cmath-tanh-Bagian6__CPP/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by Programiz
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019 */
+
+using namespace std;
+
+int main() {
+	int x = 5;
+	double result;
+
+	result = tanh(x);
+	cout << "tanh(x) = " << result << endl;
+
+	_getch();
+	return 0;
+}
